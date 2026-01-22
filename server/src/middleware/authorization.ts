6fdbc6bd77d1e9b854a7dc972729152735fa15d1
Jubayer <jubayer.shikder.007@gmail.com>
2026-01-22 09:03:26 +0600

    feat(server): add authorization middleware

---
+++ server/src/middleware/authorization.ts
@@ -0,0 +1,52 @@
+import { Request, Response, NextFunction } from "express";
+import { auth } from "../lib/auth";
+
+export enum UserRole {
+  HOST = "HOST",
+  USER = "USER",
+  ADMIN = "ADMIN",
+}
+
+const authorization = (...roles: UserRole[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const session = await auth.api.getSession({
+        headers: req.headers,
+      });
+
+      if (!session) {
+        res.status(403).json({
+          success: false,
+          message: "unauthorized access",
+        });
+      }
+
+      if (!session?.user.emailVerified) {
+        res.status(403).json({
+          success: false,
+          message: "email not verified",
+        });
+      }
+
+      req.user = {
+        id: session?.user.id!,
+        email: session?.user.email!,
+        name: session?.user.name!,
+        role: session?.user.role!,
+      };
+
+      if (roles.length && !roles.includes(req.user.role as UserRole)) {
+        res.status(403).json({
+          success: false,
+          message:
+            "Forbidden! You don't have permission to access this resources!",
+        });
+      }
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+};
+
+export default authorization;
