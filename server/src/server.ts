272ee9080c06a85e4d35a3aa86fcb3079e302b8a
Jubayer <jubayer.shikder.007@gmail.com>
2026-01-24 11:59:32 +0600

    fix(server): change server port

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,7 @@ import { prisma } from "./lib/prisma";
 import app from "./app";
 import { initWebSocket } from "./socket";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 async function main() {
   try {
